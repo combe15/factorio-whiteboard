3b4d44adf8fc213a1d1e1d9776852207cccb6830
morsk <morsk.warcraft@gmail.com>
2022-01-30 02:21:41 -0500

    script to protect space ship & crash site
    Someone does this every week, but often manually, using commands on individual pieces of the crash site. The script will do it all at once.

---
+++ Lua/protect crash site.lua
@@ -0,0 +1,19 @@
+-- Since this is probably the first command on the map, make sure to enable commands:
+
+/config set allow-commands admins-only
+
+-- And then the command:
+
+/sc --[[ protect crash site ]]
+local counts = {}
+for _,e in pairs(game.player.surface.find_entities({{-64, -32}, {32, 32}})) do
+  if e.name:match("^crash%-site") then
+    e.minable = false
+    e.destructible = false
+    counts[e.name] = (counts[e.name] or 0) + 1
+  end
+end
+game.player.print("protected:")
+for name,count in pairs(counts) do
+  game.player.print("    "..name..": "..count)
+end
