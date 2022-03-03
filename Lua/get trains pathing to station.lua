741b5b00a63bdca090edf4a860d26c80d26e736a
flbuddymooreiv <buddy.moore@gmail.com>
2022-03-03 17:24:02 -0500

    Update and rename get trains pathing to station to get trains pathing to station.lua

---
+++ Lua/get trains pathing to station.lua
@@ -0,0 +1,13 @@
+/c 
+l = ''
+stop = game.player.selected
+s = stop.position
+for k,v in pairs(game.player.surface.get_trains()) do 
+  if v.path_end_stop ~= nil then 
+    p = v.path_end_stop.position 
+    if p.x == s.x and p.y == s.y then 
+      l = l .. '[train=' .. v.locomotives.front_movers[1].unit_number .. ']'
+    end 
+  end 
+end
+game.print('Trains headed to ' .. stop.backer_name .. ' at [gps=' .. s.x .. ',' .. s.y .. ']: ' .. l)
