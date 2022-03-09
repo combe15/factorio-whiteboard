50cfd3d0150e27c3ca90dbb58be94c687faf4f83
flbuddymooreiv <buddy.moore@gmail.com>
2022-03-09 02:25:01 -0500

    Update get trains pathing to station.lua

--- Lua/get trains pathing to station.lua
+++ Lua/get trains pathing to station.lua
@@ -6,7 +6,7 @@ for k,v in pairs(game.player.surface.get_trains()) do
   if v.path_end_stop ~= nil then 
     p = v.path_end_stop.position 
     if p.x == s.x and p.y == s.y then 
-      l = l .. '[train=' .. v.locomotives.front_movers[1].unit_number .. ']'
+      l = l .. '[train=' .. v.locomotives.front_movers[1].unit_number .. '] (' .. v.path.total_distance - v.path.travelled_distance .. 'm)'
     end 
   end 
 end
