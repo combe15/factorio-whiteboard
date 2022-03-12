129dcc5850ab9546a222660dbc521bcc63bcbfeb
flbuddymooreiv <buddy.moore@gmail.com>
2022-03-12 12:16:49 -0500

    Create find recipe.lua

---
+++ Lua/find recipe.lua
@@ -0,0 +1,10 @@
+/sc 
+for k,v in pairs(game.player.surface.find_entities_filtered({name={'assembling-machine-1', 'assembling-machine-2', 'assembling-machine-3'}})) 
+do 
+  if v.get_recipe().name ~= nil and 
+    v.get_recipe().name == 'spidertron' then 
+    
+    game.print(v.get_recipe().name .. ': [gps='.. v.position.x .. ', '.. v.position.y .. ']') 
+    
+  end
+end
