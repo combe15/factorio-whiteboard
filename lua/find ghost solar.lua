c08234282ed7475e78cc67aa184e171cbf8fbeb2
flbuddymooreiv <buddy.moore@gmail.com>
2022-03-31 11:23:08 -0400

    Create find ghost solar.lua

---
+++ lua/find ghost solar.lua
@@ -0,0 +1,6 @@
+/sc 
+r = '' 
+for k,v in pairs(game.player.surface.find_entities_filtered({ghost_type={'solar-panel'}})) do 
+  r = r .. '[gps=' .. v.position.x .. ',' .. v.position.y .. ']' 
+end 
+game.print(r)
