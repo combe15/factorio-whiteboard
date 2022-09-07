217fcb2d1a440efaebddf50a78ccc48280430bea
flbuddymooreiv <buddy.moore@gmail.com>
2022-09-07 11:31:04 -0400

    Update scan_player_inventories.lua
    fix the thing that doesn't do what it is supposed to do

--- lua/scan_player_inventories.lua
+++ lua/scan_player_inventories.lua
@@ -1,10 +1,16 @@
-/c 
-local x='' 
-for k,v in pairs(game.players) do 
-  if v.get_inventory(defines.inventory.character_main) ~= nil then 
-    x = x .. ', ' .. v.name  .. ': ' .. v.get_inventory(defines.inventory.character_main).count_empty_stacks() .. '/' .. #v
-  else
-    x = x .. ', ' .. v.name .. ': ??/??'
-  end
+/c  
+local x=''  
+for k,v in pairs(game.players) do    
+  if not v.connected then     
+    inv = v.get_inventory(defines.inventory.character_main)     
+    if inv ~= nil then        
+      if inv.count_empty_stacks() ~= #inv then         
+        x = x .. ', ' .. v.name  .. ': ' .. inv.count_empty_stacks() .. '/' .. #inv       
+      end     
+    else       
+      #ragequit detector (could also be admin horseplay)
+      x = x .. ', ' .. v.name .. ': ??/??'     
+    end   
+  end  
 end 
-game.print(x)
+game.print(x) 
