b0d8b9030620250fe99a7d9c587e8358214149c5
flbuddymooreiv <buddy.moore@gmail.com>
2022-09-10 00:57:02 -0400

    Update scan_player_inventories.lua
    lua comments not c duh

--- lua/scan_player_inventories.lua
+++ lua/scan_player_inventories.lua
@@ -1,16 +1,19 @@
 /c  
+
 local x=''  
+
 for k,v in pairs(game.players) do    
   if not v.connected then     
     inv = v.get_inventory(defines.inventory.character_main)     
     if inv ~= nil then        
       if inv.count_empty_stacks() ~= #inv then         
-        x = x .. ', ' .. v.name  .. ': ' .. inv.count_empty_stacks() .. '/' .. #inv       
+        x = x .. ', ' .. v.name  .. ': ' .. inv.count_empty_stacks() .. '/' .. #inv
       end     
     else       
-      #ragequit detector (could also be admin horseplay)
+      -- ragequit detector (could also be admin horseplay)
       x = x .. ', ' .. v.name .. ': ??/??'     
     end   
   end  
-end 
+end
+
 game.print(x) 
