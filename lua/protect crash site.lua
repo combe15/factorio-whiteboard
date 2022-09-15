6e43bafbf69df8576f1d893e531288b7ea23108f
flbuddymooreiv <buddy.moore@gmail.com>
2022-09-14 20:59:13 -0400

    Update protect crash site.lua
    close comments

--- lua/protect crash site.lua
+++ lua/protect crash site.lua
@@ -2,14 +2,14 @@
   Make the crash site as an indestructible object.
 
   Since this is probably the first command on the map, make sure to enable commands:
-]]
+--]]
 /config set allow-commands admins-only
 
 --[[
   And then the command:
-]]
+--]]
 
-/silent-command --[[ protect crash site ]]
+/silent-command --[[ protect crash site --]]
 local counts = {}
 for _,e in pairs(game.player.surface.find_entities({{-64, -32}, {32, 32}})) do
   if e.name:match("^crash%-site") then
