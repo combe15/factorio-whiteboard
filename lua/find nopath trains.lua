5b69cd2f138a0cc0d7720b94e71e0ede4c9495ea
flameSla <39356103+flameSla@users.noreply.github.com>
2023-12-18 23:51:08 +0700

    Update find nopath trains.lua

--- lua/find nopath trains.lua
+++ lua/find nopath trains.lua
@@ -1,9 +1,25 @@
 /silent-command
 --[[
-  Print out what trains currently report no path
+  no path station finder
 ]]
-for k,v in pairs(game.player.surface.get_trains()) do
-  if v.state == defines.train_state.no_path then
-    game.print('[train=' .. v.locomotives.front_movers[1].unit_number .. ']')
-  end
+local st = {}
+for k, v in pairs(game.player.surface.get_trains()) do
+    if v.state == defines.train_state.no_path then
+        game.print("[train=" .. v.locomotives.front_movers[1].unit_number .. "]")
+        st[v.schedule.records[v.schedule.current].station] = true
+    end
 end
+game.print("no path stations:")
+game.print(game.table_to_json(st))
+local found, out = 0, "\n"
+for _, e in pairs(game.player.surface.find_entities_filtered {type = {"train-stop"}}) do
+    for st_name, _ in pairs(st) do
+        if st_name == e.backer_name then
+            if e.trains_limit > e.trains_count then
+                found = found + 1
+                out = out .. found .. ":[gps=" .. e.position.x .. "," .. e.position.y .. "] "
+            end
+        end
+    end
+end
+game.player.print(out ~= "" and out or "None found.")
